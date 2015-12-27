ec6c1d148208676c22768de660dc867aa34abe2f
boni <boni@twine.hu>
2015-12-27 14:30:23 +0100

    initial commit after auth

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,45 @@
+defmodule Rumbl.UserController do
+  use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
+  alias Rumbl.User
+
+  def index(conn, _params) do
+    felhasznalok = Repo.all(User)
+    render conn, "index.html", users: felhasznalok
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+    render conn, "show.html", user: user
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.registration_changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
+
+end
